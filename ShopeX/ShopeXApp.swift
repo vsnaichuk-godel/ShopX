1b1328f7fb82790ddcac9f25cbe05065218d157c
Volodymyr Snaichuk <v.snaichuk@godeltech.com>
2026-01-14 07:35:07 +0100

    Initial commit

---
+++ ShopeX/ShopeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopeXApp.swift
+//  ShopeX
+//
+//  Created by Volodymyr Snaichuk on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShopeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopeXTests/ShopeXTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopeXTests.swift
+//  ShopeXTests
+//
+//  Created by Volodymyr Snaichuk on 04/01/2026.
+//
+
+import Testing
+@testable import ShopeX
+
+struct ShopeXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
